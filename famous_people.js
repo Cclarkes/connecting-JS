4591dc74e3bea2286c326caaf668d6377e68b5ae
CClarkes <connorclarkes@gmail.com>
2019-02-26 20:19:20 +0000

    Simple query for famous people complete

---
+++ famous_people.js
@@ -0,0 +1,15 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+const firstEntry = process.argv[2];
+const lastEntry = process.argv[3];
+const client = new pg.Client(settings);
+
+client.connect((err, db) => {
+    if(err) {
+        console.error(("Connection Error"), err);
+    }
+    console.log("hello");
+    db.query("SELECT * FROM famous_people WHERE first_name LIKE '" + firstEntry + "'", (err, res) => {
+        console.log(res.rows);
+    })
+});
